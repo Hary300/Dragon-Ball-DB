4741c1f9149044bae2206ff36505ecfbb2ccfa31
Haryadi Nugraha Putra <h.nugraha.p@gmail.com>
2026-04-17 23:50:34 +0700

    feat: getData API testing

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("this is Hary");
+const url = "https://dragonball-api.com/api/characters";
+
+async function init() {
+  const data = await getData(url);
+  console.log(data);
+}
+
+async function getData(url) {
+  try {
+    const res = await axios(url);
+    return res.data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+init();
